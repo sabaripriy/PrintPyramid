5c84205d166ee31d31b7c42bdbf880e9232320b1
sabaripriy <85024788+sabaripriy@users.noreply.github.com>
2021-05-29 22:56:45 +0530

    Update Program.cs

--- PrintPyramid/Program.cs
+++ PrintPyramid/Program.cs
@@ -5,9 +5,9 @@ public class PrintExample
     {
         char ch = 'A';
         int i, j, k, m;
-        for (i = 1; i <= 5; i++)
+        for (i = 1; i <= 4; i++)
         {
-            for (j = 5; j >= i; j--)
+            for (j = 4; j >= i; j--)
                 Console.Write(" ");
             for (k = 1; k <= i; k++)
                 Console.Write(ch++ );
